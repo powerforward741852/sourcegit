4305475a26b5154bb87ebbfa2eee4798ad2b4666
qinrong <545912655@qq.com>
2017-09-01 21:35:35 +0800

    初始化项目

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by 秦榕 on 2017/9/1.
+//  Copyright © 2017年 秦榕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
