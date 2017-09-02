903c4909018973fb82d7d5104e9ebff55e211a20
qinrong <545912655@qq.com>
2017-09-02 20:37:42 +0800

    Revert "Revert "后悔操作""
    This reverts commit b316bdb7b562898416337e45be38190a48235cb6.

--- gitTest/gitTest/ViewController.m
+++ gitTest/gitTest/ViewController.m
@@ -14,6 +14,20 @@
 
 @implementation ViewController
 
+//新增代码
+- (instancetype)initWithDict:(NSDictionary*)dict
+{
+    if (self = [super init]) {
+        [self setValuesForKeysWithDictionary:dict];
+    }
+    return self;
+    
+}
++ (instancetype)ssssWithDict:(NSDictionary*)dict
+{
+    
+    return [[self alloc] initWithDict:dict];
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
