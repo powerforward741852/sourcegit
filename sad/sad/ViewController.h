f4f02c94692ba74cc3694b3faa2dad78f5e39701
qinrong <545912655@qq.com>
2017-09-03 13:29:20 +0800

    Revert "移除"
    This reverts commit 5cc6bf76a7d88e39912ab89cc4c3c7193a1d605d.

---
+++ sad/sad/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  sad
+//
+//  Created by 秦榕 on 2017/9/3.
+//  Copyright © 2017年 秦榕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property(strong,nonatomic)NSArray* asdasd;
+
+
+@end
+
---
+++ sad/sad/ViewController.m
@@ -0,0 +1,37 @@
+//
+//  ViewController.m
+//  sad
+//
+//  Created by 秦榕 on 2017/9/3.
+//  Copyright © 2017年 秦榕. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+}
+-(void)takepic1{
+
+}
+
+-(void)takepic2{
+
+}
+
+//goodmoney
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
